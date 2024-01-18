4b2a6d125c41df87bbe3531963d82f8810f2aeb8
unknown <daniielaalj>
2024-01-18 10:20:11 +0200

    Init commit

---
+++ 3uzd.py
@@ -0,0 +1,13 @@
+def lasit_teksta_failu():
+    try:
+     with open('r') as faila_atslega:
+        rindas = faila_atslega.readlines()
+        if len(rindas) >= 3:
+            tresa_rinda = rindas[2].strip()
+            print(tresa_rinda)
+        else:
+            print("Failā ir mazāk par trīs rindiņām.")
+    except FileNotFoundError:
+       print(f"Teksta fails ar nosaukumu '' nav atrasts.")
+    except:
+       print("Kļūda nolasot teksta failu.")
